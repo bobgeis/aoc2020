62bfdb7273ba9a59a6ab05b8129367b66849b4cc
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-01 17:18:01 -0500

    lib: move readIntLines to aocutils

--- src/lib/aocutils.nim
+++ src/lib/aocutils.nim
@@ -1,7 +1,9 @@
 
 ## This files useful for this advent of code repo.  The classic example is getting the input file.
 
-import std/[strformat]
+import std/[sequtils,strformat,strutils]
+
+import lib/[bedrock]
 
 const
   inputDir = "data"
@@ -11,3 +13,7 @@ proc inputFilePath*(day: string): string =
 
 proc inputTestFilePath*(day: string, test: int): string =
   &"{inputDir}/i{day}t{test}.txt"
+
+proc readIntLines*(path:string):seq[int] =
+  ## for reading in a text file of ints separated by newlines
+  path.getlines.map(parseInt)
