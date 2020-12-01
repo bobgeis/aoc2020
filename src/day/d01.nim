2be82dbc7111dc8e540c6efb17499d631a8455b1
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-01 17:17:57 -0500

    d01: catch duplicates, add timings

--- src/day/d01.nim
+++ src/day/d01.nim
@@ -21,24 +21,40 @@ proc testFile(i: int): string = inputTestFilePath(dayNum, i)
 let nums = inputFile.readIntLines
 
 proc part1*(): int =
+  defer: doAssert 1018944 == result
   for n in nums:
     for m in nums:
       let sum = n + m
-      if sum == 2020:
-        result = n * m
-  assert 1018944 == result
+      if sum == 2020 and n != m:
+        return n * m
 
 proc part2*(): int =
+  defer: doAssert 8446464 == result
   for n in nums:
     for m in nums:
       for o in nums:
         let sum = n + m + o
-        if sum == 2020:
-          result = n * m * o
-  assert 8446464 == result
+        if sum == 2020 and n != m and m != o and o != n:
+          return n * m * o
 
 when isMainModule:
   echo &"Day{dayNum}"
-  echo &"Part1 {part1()}"
-  echo &"Part2 {part2()}"
+  timeit " ":
+    echo &"Part1 {part1()}"
+  timeit " ":
+    echo &"Part2 {part2()}"
 
+#[
+## Timing for brute force approach (full loops which are O(n^2) and O(n^3) respectively):
+$ nim c --gc:arc -d:danger --opt:speed src/day/d01.nim
+$ time out/runme
+Day01
+Part1 1018944
+  in 15 microseconds and 271 nanoseconds
+Part2 8446464
+  in 1 millisecond, 990 microseconds, and 73 nanoseconds
+
+real    0m0.007s
+user    0m0.003s
+sys     0m0.002s
+]#
