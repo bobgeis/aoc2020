362468cc2fe844f6cdd5470567a80f3422c2d34e
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-01 18:57:32 -0500

    d01: use indices

--- src/day/d01.nim
+++ src/day/d01.nim
@@ -20,20 +20,20 @@ proc testFile(i: int): string = inputTestFilePath(dayNum, i)
 
 proc part1*(nums:seq[int]): int =
   defer: doAssert 1018944 == result
-  for n in nums:
-    for m in nums:
-      let sum = n + m
-      if sum == 2020 and n != m:
-        return n * m
+  for i in 0..nums.high:
+    for j in i..nums.high:
+      let sum = nums[i] + nums[j]
+      if sum == 2020 and i != j:
+        return nums[i] * nums[j]
 
 proc part2*(nums:seq[int]): int =
   defer: doAssert 8446464 == result
-  for n in nums:
-    for m in nums:
-      for o in nums:
-        let sum = n + m + o
-        if sum == 2020 and n != m and m != o and o != n:
-          return n * m * o
+  for i in 0..nums.high:
+    for j in i..nums.high:
+      for k in j..nums.high:
+        let sum = nums[i] + nums[j] + nums[k]
+        if sum == 2020  and i != j and i != k and k != j:
+          return nums[i] * nums[j] * nums[k]
 
 when isMainModule:
   echo &"Day{dayNum}"
@@ -46,16 +46,15 @@ when isMainModule:
 
 #[
 ## Timing for brute force approach (full loops which are O(n^2) and O(n^3) respectively):
-$ nim c --gc:arc -d:danger --opt:speed src/day/d01.nim
-$ time out/runme
+$ nim c --gc:arc -d:danger --opt:speed src/day/d01.nim && time out/runme
 Day01
-Read file in 176 microseconds and 153 nanoseconds
+Read file in 140 microseconds and 137 nanoseconds
 Part1 is 1018944
-  in 6 microseconds and 596 nanoseconds
+  in 5 microseconds and 634 nanoseconds
 Part2 is 8446464
-  in 1 millisecond, 944 microseconds, and 947 nanoseconds
+  in 978 microseconds and 547 nanoseconds
 
-real    0m0.006s
-user    0m0.003s
+real    0m0.004s
+user    0m0.002s
 sys     0m0.002s
 ]#
