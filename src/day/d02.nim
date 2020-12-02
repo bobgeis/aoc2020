a06e60fc3b163c28c3f19b95101cb40a505d2c8f
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-02 09:21:11 -0500

    d02: solved

--- src/day/d02.nim
+++ src/day/d02.nim
@@ -13,31 +13,79 @@ import pkg/[itertools, memo, stint]
 import lib/[aocutils, bedrock, graphwalk, shenanigans, vecna]
 
 const
+  githash = staticexec "git rev-parse --short HEAD"
   dayNum = "02"
-  inputFile = "data/i02.txt"
+  inputFile = &"data/i{dayNum}.txt"
 
 proc testFile(i: int): string = inputTestFilePath(dayNum, i)
 
-proc part1*(): int =
-  # defer: doAssert xxx == result
-  result = 1
+type Input = tuple
+  min,max:int
+  c:char
+  pw:string
 
-proc part2*(): int =
-  # defer: doAssert xxx == result
-  result = 2
+proc scanline(s:string):Input =
+  var
+    minCount,maxCount:int
+    letter:string
+    pw:string
+  if s.scanf("$i-$i $w: $w",minCount,maxCount,letter,pw):
+    return (minCount,maxCount,letter[0],pw)
+
+proc parse(path:string):seq[Input] =
+  for line in path.lines:
+    result.add line.scanline
+
+proc valid(inp:Input):bool =
+  var count = 0
+  for c in inp.pw:
+    if c == inp.c:
+      inc count
+  count in inp.min..inp.max
+
+proc valid2(inp:Input):bool =
+  (inp.pw[inp.min-1] == inp.c) xor (inp.pw[inp.max-1] == inp.c)
+
+proc part1*(inputs:seq[Input]): int =
+  defer: doAssert 569 == result
+  for inp in inputs:
+    if inp.valid:
+      inc result
+
+proc part2*(inputs:seq[Input]): int =
+  defer: doAssert 346 == result
+  for inp in inputs:
+    if inp.valid2:
+      inc result
 
 when isMainModule:
-  echo &"Day{dayNum}"
-  var val1:int
-  timeit &"Part1 is {val1}":
-    val1 = part1()
-  var val2:int
-  timeit &"Part2 is {val2}":
-    val2 = part2()
+  echo &"Day{dayNum} at #{githash}"
+  var inputs:seq[Input]
+  timeit "Read file and parse":
+    inputs = inputFile.parse
+  var res1:int
+  timeit &"Part1 is {res1}":
+    res1 = part1(inputs)
+  var res2:int
+  timeit &"Part2 is {res2}":
+    res2 = part2(inputs)
 
 #[
 ## Compiler commands:
 nim r src/day/d02.nim
 nim c --gc:arc -d:danger --opt:speed src/day/d02.nim && time out/runme
 nim check --warnings:on --hints:on src/day/d01.nim
+]#
+
+#[
+  First attempt
+$ nim c --gc:arc -d:danger --opt:speed src/day/d02.nim && time out/runme
+Day02 at #19c4133
+Read file and parse in 517 microseconds and 801 nanoseconds
+Part1 is 569 in 17 microseconds and 442 nanoseconds
+Part2 is 346 in 1 microsecond and 794 nanoseconds
+
+real    0m0.004s
+user    0m0.002s
+sys     0m0.001s
 ]#
