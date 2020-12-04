c9661529a87b5b59a7dac79c982f661dc4a21230
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-04 17:51:59 -0500

    d04: use template to reduce repitition

--- src/day/d04.nim
+++ src/day/d04.nim
@@ -6,7 +6,7 @@
 import std/[ sequtils, sets, strformat, strscans, strutils, unittest]
 
 # local lib modules: src/lib/
-import lib/[aocutils, bedrock, shenanigans]
+import lib/[aocutils, shenanigans]
 
 const
   day = "04"
@@ -43,38 +43,28 @@ const
   eyecolors = ["amb","blu","brn","gry","grn","hzl","oth"].toHashSet
   chars = {'0'..'9','a'..'f'}
 
+template checkKey(key:string,body:untyped):untyped =
+  if k == key:
+    if body:
+      inc reqs
+    else:
+      return false
+
 proc checkpp2(s:string):bool =
   var reqs = 0
   for kv in s.split({'\n',' '}):
     [k,v] ..= kv.split(':')
-    case k
-    of "byr":
-      if v.parseInt in 1920..2002: inc reqs
-      else: return false
-    of "iyr":
-      if v.parseInt in 2010..2020: inc reqs
-      else: return false
-    of "eyr":
-      if v.parseInt in 2020..2030: inc reqs
-      else: return false
-    of "hgt":
+    checkKey "byr", v.parseInt in 1920..2002
+    checkKey "iyr", v.parseInt in 2010..2020
+    checkKey "eyr", v.parseInt in 2020..2030
+    checkKey "hgt":
       var
         h:int
         u:string
-      if v.scanf("$i$w",h,u) and
-      ((u == "in" and h in 59..76) or (u == "cm" and h in 150..193)):
-        inc reqs
-      else: return false
-    of "hcl":
-      if v.len == 7 and v[0] == '#' and v[1..v.high].allit(it in chars):
-        inc reqs
-      else: return false
-    of "ecl":
-      if v in eyecolors: inc reqs
-      else: return false
-    of "pid":
-      if v.len == 9 and v.parseInt >= 0: inc reqs
-      else: return false
+      v.scanf("$i$w",h,u) and ((u == "in" and h in 59..76) or (u == "cm" and h in 150..193))
+    checkKey "hcl", v.len == 7 and v[0] == '#' and v[1..v.high].allit(it in chars)
+    checkKey "ecl", v in eyecolors
+    checkKey "pid", v.len == 9 and v.parseInt >= 0
   if reqs == 7:
     return true
 
@@ -159,6 +149,20 @@ Day04 for in/i04.txt
 Read file in 179 microseconds and 11 nanoseconds
 Part1 is 182 in 406 microseconds and 162 nanoseconds
 Part2 is 109 in 332 microseconds and 867 nanoseconds
+
+real    0m0.004s
+user    0m0.002s
+sys     0m0.001s
+]#
+
+#[
+  Use template to reduce reptition. Slightly slower than case-of version, but it's harder to construct a template to generate correct of expressions (I'm sure it's possible in a macro).
+$ nim c --gc:arc -d:danger --opt:speed $DAY && time out/run
+
+Day04 for in/i04.txt
+Read file in 174 microseconds and 43 nanoseconds
+Part1 is 182 in 404 microseconds and 558 nanoseconds
+Part2 is 109 in 345 microseconds and 443 nanoseconds
 
 real    0m0.004s
 user    0m0.002s
