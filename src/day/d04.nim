6af914c9e28819329dedfaadde8ee2b59d4f5e8f
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-04 14:31:14 -0500

    d04: solved

--- src/day/d04.nim
+++ src/day/d04.nim
@@ -3,7 +3,7 @@
 ## https://adventofcode.com/2020/day/4
 
 # std lib modules: https://nim-lang.org/docs/lib.html
-import std/[ algorithm, deques, math, options, os, parsecsv, sequtils, sets, strformat, strscans, strtabs, strutils, sugar, tables, unittest]
+import std/[ algorithm, deques, math, memfiles, options, os, parsecsv, sequtils, sets, strformat, strscans, strtabs, strutils, sugar, tables, unittest]
 
 # nimble pkgs: https://nimble.directory/
 import pkg/[itertools, memo, stint]
@@ -15,18 +15,113 @@ const
   githash = staticexec "git rev-parse --short HEAD"
   day = "04"
   inPath = inputPath(day)
+  testPath = inputPath("04t1")
 
-proc getPath():string = commandLineParams().getOr(0,inPath)
+#[
+byr (Birth Year)
+iyr (Issue Year)
+eyr (Expiration Year)
+hgt (Height)
+hcl (Hair Color)
+ecl (Eye Color)
+pid (Passport ID)
+cid (Country ID)
+]#
+
+type
+  PassPort = object
+    byr,iyr,eyr,hgt,hcl,ecl,pid,cid:string
+    valid:bool
+
+proc readpp(s:string):PassPort =
+  var reqs = 0
+  for kv in s.split({'\n',' '}):
+    [k,v] ..= kv.split(':')
+    case k
+    of "byr":
+      result.byr = v
+      inc reqs
+    of "iyr":
+      result.iyr = v
+      inc reqs
+    of "eyr":
+      result.eyr = v
+      inc reqs
+    of "hgt":
+      result.hgt = v
+      inc reqs
+    of "hcl":
+      result.hcl = v
+      inc reqs
+    of "ecl":
+      result.ecl = v
+      inc reqs
+    of "pid":
+      result.pid = v
+      inc reqs
+    of "cid":
+      result.byr = v
+    else:
+      err &"unrecognized passport key{k}"
+  if reqs == 7:
+    result.valid = true
+
+const
+  eyecolors = ["amb","blu","brn","gry","grn","hzl","oth"].toHashSet
+  chars = {'0'..'9','a'..'f'}
+
+proc readpp2(s:string):PassPort =
+  var reqs = 0
+  for kv in s.split({'\n',' '}):
+    [k,v] ..= kv.split(':')
+    case k
+    of "byr":
+      if v.parseInt in 1920..2002:
+        result.byr = v
+        inc reqs
+    of "iyr":
+      if v.parseInt in 2010..2020:
+        result.iyr = v
+        inc reqs
+    of "eyr":
+      if v.parseInt in 2020..2030:
+        result.eyr = v
+        inc reqs
+    of "hgt":
+      var
+        h:int
+        u:string
+      if v.scanf("$i$w",h,u) and
+      ((u == "in" and h in 59..76) or (u == "cm" and h in 150..193)):
+        result.hgt = v
+        inc reqs
+    of "hcl":
+      if v.len == 7 and v[0] == '#' and v[1..v.high].allit(it in chars):
+        result.hcl = v
+        inc reqs
+    of "ecl":
+      if v in eyecolors:
+        result.ecl = v
+        inc reqs
+    of "pid":
+      if v.len == 9 and v.parseInt >= 0:
+        result.pid = v
+        inc reqs
+    of "cid":
+      result.byr = v
+    else:
+      err &"unrecognized passport key{k}"
+  if reqs == 7:
+    result.valid = true
 
 proc parse*(path:string): seq[string] =
-  result = path.getlines
-  result.doit(echo it)
+  result = path.readFile.split("\n\n")
 
 proc part1*(input:seq[string]): int =
-  result = 1
+  input.countit(it.readpp.valid)
 
 proc part2*(input:seq[string]): int =
-  result = 2
+  input.countit(it.readpp2.valid)
 
 proc run*(path:string=inPath) =
   echo &"Day{day} for {path}"
@@ -39,10 +134,13 @@ proc run*(path:string=inPath) =
   var res2:int
   timeit &"Part2 is {res2}":
     res2 = part2(input)
-  # case path
-  # of inPath:
-  #   check res2 == ?
-  #   check res1 == ?
+  case path
+  of inPath:
+    check res1 == 182
+    check res2 == 109
+  of testPath:
+    check res1 == 2
+    check res2 == 2
 
 when isMainModule:
   var paths = getCliPaths(default=inPath)
