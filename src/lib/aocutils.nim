0cae5ca51266b32faab0e08ad1ed88d7429ca924
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-05 19:16:28 -0500

    lib: add timevar, makeRunProc templates

--- src/lib/aocutils.nim
+++ src/lib/aocutils.nim
@@ -1,7 +1,7 @@
 
 ## This files useful for this advent of code repo.  The classic example is getting the input file.
 
-import std/[os,sequtils,strformat,strutils]
+import std/[monotimes,os,sequtils,strformat,strutils,times]
 
 import lib/[bedrock]
 
@@ -23,3 +23,44 @@ proc getCliPaths*(default:string):seq[string] =
 proc readIntLines*(path:string):seq[int] =
   ## for reading in a text file of ints separated by newlines
   path.getlines.map(parseInt)
+
+type
+  RunResult* = tuple
+    day: string
+    path: string
+    res: array[2,int]
+    dur: array[4,Duration]
+
+template makeRunProc*():untyped =
+# template runit*(part0:untyped):untyped =
+# template runit*(part0,part1,part2:untyped):untyped =
+  proc run*(path:string=inPath):RunResult =
+    timevar durall:
+      timevar dur0:
+        let input = part0(path)
+      var res1:int
+      timevar dur1:
+        res1 = part1(input)
+      var res2:int
+      timevar dur2:
+        res2 = part2(input)
+      if path in checkpart1:
+        check checkpart1[path] == res1
+      if path in checkpart2:
+        check checkpart2[path] == res2
+    return (day:day,path:path,res:[res1,res2],dur:[dur0,dur1,dur2,durAll])
+
+
+proc echoRR*(rr:RunResult) =
+  echo ""
+  echo &"Day {rr.day}"
+  echo ""
+  echo &"Answers for {rr.path}"
+  echo &"Part1: {rr.res[0]}"
+  echo &"Part2: {rr.res[1]}"
+  echo ""
+  echo &"Times:"
+  echo &"Part0: {rr.dur[0].inNanoseconds.float / 1e6:>9.3f}ms"
+  echo &"Part1: {rr.dur[1].inNanoseconds.float / 1e6:>9.3f}ms"
+  echo &"Part2: {rr.dur[2].inNanoseconds.float / 1e6:>9.3f}ms"
+  echo &"Total: {rr.dur[3].inNanoseconds.float / 1e6:>9.3f}ms"
--- src/lib/shenanigans.nim
+++ src/lib/shenanigans.nim
@@ -48,6 +48,26 @@ proc echoMonoTime(t: Duration, msg: string = "", iters: int = 1) =
   timeStr &= $t
   echo timeStr
 
+template timevar*(dur:untyped, body:untyped):untyped =
+  ## A timing template, it takes the name of a var that it will declare as a Duration.
+  ## Durations are in std/times
+  ## Adapted from Kaynato's https://github.com/Kaynato/AdventOfCode/blob/master/timeit.nim
+  var dur:Duration
+  let t1 = getMonoTime()
+  body
+  let t2 = getMonoTime()
+  dur = t2 - t1
+
+template timeadd*(dur:var Duration, body:untyped):untyped =
+  ## A timing template, it takes the name of a var Duration that it will add to
+  ## Durations are in std/times
+  ## Adapted from Kaynato's https://github.com/Kaynato/AdventOfCode/blob/master/timeit.nim
+  let t1 = getMonoTime()
+  body
+  let t2 = getMonoTime()
+  dur += t2 - t1
+
+
 template timeit*(msg: string = "", iters: int = 1, body: untyped): untyped =
   ## A handy timing template,
   ## Adapted from Kaynato's https://github.com/Kaynato/AdventOfCode/blob/master/timeit.nim
