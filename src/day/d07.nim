8e225d7a1b95081ccf2f092cd8827cb57bc60816
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-07 14:06:27 -0500

    d07: solved

--- src/day/d07.nim
+++ src/day/d07.nim
@@ -14,21 +14,70 @@ import lib/[aocutils, bedrock, graphwalk, shenanigans, vecna]
 const
   day = "07"
   inPath = inputPath(day)
+  testPath = inputPath("07t1")
   checkpart1 = {
-    "path":0,
+    inpath:119,
+    testPath:4,
     }.toTable
   checkpart2 = {
-    "path":0,
+    inpath:155802,
+    testPath:32,
     }.toTable
 
-proc part0*(path:string): seq[string] =
-  path.getLines
+const
+  myBag = "shiny gold"
 
-proc part1*(input:seq[string]): int =
-  result = 0
+type
+  Bag = Table[string,int]
+  BagTab = Table[string,Bag]
 
-proc part2*(input:seq[string]): int =
-  result = 0
+proc scanline(s:string, contains,containedby: var BagTab) =
+  var
+    b,contents:string
+    tab = initTable[string,int]()
+  if s.scanf("$+ bags contain $+",b,contents):
+    if b notin containedby:
+      containedby[b] = initTable[string,int]()
+    for content in contents.split(", "):
+      var
+        num:int
+        name:string
+      if content.scanf("$i $+ bag",num,name):
+        tab[name] = num
+        if name notin containedby:
+          containedby[name] = initTable[string,int]()
+        containedby[name][b] = num
+      elif content == "no other bags.": discard
+      else: err &"Could not scanf content: '{content}'"
+    contains[b] = tab
+  else: err &"Could not scanline: '{s}'"
+
+proc part0*(path:string): (BagTab,BagTab) =
+  var
+    contains = initTable[string,Bag]()
+    containedby = initTable[string,Bag]()
+  for line in path.getLines:
+    line.scanline(contains,containedby)
+  return (contains,containedby)
+
+proc part1*(input:(BagTab,BagTab)): int =
+  let bt = input[1]
+  proc adjs(s:string):seq[string] =
+    toSeq(bt[s].keys)
+  bfs(mybag,adjs).len - 1 # - 1 because mybag doesn't contain itself
+
+proc part2*(input:(BagTab,BagTab)): int =
+  let bt = input[0]
+  var
+    q = @[(mybag,1)]
+    visited = initHashSet[string]()
+    count = 0
+  while q.len > 0:
+    [name, num] ..= q.pop
+    visited.incl name
+    count += num
+    q.add toSeq(bt[name].pairs).mapit((it[0],it[1]*num))
+  return count - 1 # - 1 because mybag doesn't contain itself
 
 makeRunProc()
 
