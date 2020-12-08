734815340fcd42c1245ee03acdf582104ea108bf
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-07 21:52:43 -0500

    d08: setup

---
+++ day/d08.nim
@@ -0,0 +1,19 @@
+import lib/[imps]
+const
+  day = "08"
+  inPath = inputPath(day)
+  # inpath.part1is 1
+  # inpath.part2is 2
+
+proc part0*(path: string): seq[string] =
+  path.getLines
+
+proc part1*(input: seq[string]): int =
+  result = 0
+
+proc part2*(input: seq[string]): int =
+  result = 0
+
+makeRunProc()
+when isMainModule: getCliPaths(inPath).doit(it.run.echoRR)
+
