2a812541c24e79815db8de785af135d645a332e1
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-10 17:29:48 -0500

    d10: solved

--- day/d10.nim
+++ day/d10.nim
@@ -3,27 +3,31 @@ const
   day = "10"
   inPath = inputPath(day)
   testPath = inputPath("10t1")
+  testPath2 = inputPath("10t2")
 
 inpath.part1is 1836
-# inpath.part2is 2
+inpath.part2is 43406276662336.int
 testpath.part1is 220
 testpath.part2is 19208
+testpath2.part2is 8
 
 proc part0*(path: string): seq[int] =
   path.readIntLines.sorted
 
 proc part1*(input: seq[int]): int =
-  var
-    ones = 1    # +1 for the outlet
-    threes = 1  # +1 for the device
+  var ones, threes = 1 # ones=1 for outlet, threes=1 for device
   for i in 1..input.high:
-    let diff = input[i] - input[i-1]
-    if diff == 1: ones.inc
-    elif diff == 3: threes.inc
+    case input[i] - input[i-1]
+    of 1: ones.inc
+    of 3: threes.inc
+    else: discard
   return ones * threes
 
 proc part2*(input: seq[int]): int =
-  result = 0
+  var paths = [0].toCountTable
+  for val in input:
+    paths[val] = paths[val-1] + paths[val-2] + paths[val-3]
+  return paths[input[^1]]
 
 makeRunProc()
 when isMainModule: getCliPaths(inPath).doit(it.run.echoRR)
