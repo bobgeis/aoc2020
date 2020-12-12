eaa5cb09b05dbd6a527ed91ebb595d1e4c27b692
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-12 18:28:24 -0500

    d12: setup, add input

---
+++ day/d12.nim
@@ -0,0 +1,23 @@
+import lib/[imps]
+const
+  day = "12"
+  inPath = inputPath(day)
+  testPath = inputPath(day,"t1")
+
+# inpath.part1is 1
+# inpath.part2is 2
+testPath.part1is 25
+
+proc part0*(path: string): seq[string] =
+  path.getLines
+
+proc part1*(input: seq[string]): int =
+  input.doit(it.echo)
+  result = 0
+
+proc part2*(input: seq[string]): int =
+  result = 0
+
+makeRunProc()
+when isMainModule: getCliPaths(day).doit(it.run.echoRR)
+
