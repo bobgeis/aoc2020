fbb0b768f4bea875040df98fd03f2e8a06647b90
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-13 13:47:16 -0500

    d13: solved

--- day/d13.nim
+++ day/d13.nim
@@ -3,11 +3,13 @@ const
   day = "13"
   inPath = inputPath(day)
   testPath = inputPath(day,"t1")
+  testPath2 = inputPath(day,"t2")
 
 inpath.part1is 205
-# inpath.part2is 2
+inpath.part2is 803025030761664.int
 testPath.part1is 295
 testPath.part2is 1068781
+testPath2.part2is 3417
 
 proc parsebuses(s:string):seq[int] =
   for bus in s.split(','):
@@ -23,21 +25,41 @@ proc parse1(ss:seq[string]):(int,seq[int]) =
   result[1] = ss[1].parsebuses
 
 proc part1*(input:seq[string]): int =
-  let (dept,buses) = input.parse1
+  let (deptr,buses) = input.parse1
   var
     wait = int.high
     id = 0
   for i in buses:
     let
-      d = dept div i
-      w = (d + 1) * i - dept
+      d = deptr div i
+      w = (d + 1) * i - deptr
     if w < wait:
       wait = w
       id = i
   return id * wait
 
+proc parse2(s:string):seq[(int,int)] =
+  let ss = s.split(',')
+  for i,bus in ss:
+    var b:int
+    if bus.parseint(b,0) > 0:
+      result.add (i,b)
+
 proc part2*(input:seq[string]): int =
-  result = 0
+  ## Once we sync any two buses correctly,
+  ## then we can treat them as a single larger bus with a longer period, lcm(a,b).
+  ## We can do this repeatedly until we have a single large "bus".
+  ## The time that this "bus" first departs is our answer.
+  var period = 1
+  for (i,bus) in input[1].parse2:
+    let
+      wait = (bus-i).pmod(bus) # account for the offset using python's mod
+      newper = period.lcm(bus)
+    for j in result.countup(newper, period):
+      if j mod bus == wait:
+        result = j
+        period = newper
+        break
 
 makeRunProc()
 when isMainModule: getCliPaths(day).doit(it.run.echoRR)
