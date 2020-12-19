b29a4b2ee1554a5cde14f0df03266cbfe895cc09
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-18 21:54:42 -0500

    d15: solve part 1

--- day/d15.nim
+++ day/d15.nim
@@ -4,19 +4,31 @@ const
   inPath = inputPath(day)
   testPath = inputPath(day,"t1")
 
-# inpath.part1is 1
+inpath.part1is 387
 # inpath.part2is 2
 testPath.part1is 436
 # testPath.part2is 2
 
-proc part0*(path: string): seq[string] =
-  path.readfile.split(',')
+proc part0*(path: string): seq[int] =
+  path.readfile.split(',').mapit(it.parseInt)
 
-proc part1*(input: seq[string]): int =
-  input.doit(it.echo)
-  result = 0
+proc part1*(input: seq[int]): int =
+  var
+    tab = initCountTable[int]()
+    last = input[^1]
+  for i,num in input:
+    tab[num] = i + 1
+  for i in input.len+1..2020:
+    let lasttime = tab[last]
+    if lasttime == 0:
+      tab[last] = i - 1
+      last = 0
+    else:
+      tab[last] = i - 1
+      last = i - 1 - lasttime
+  result = last
 
-proc part2*(input: seq[string]): int =
+proc part2*(input: seq[int]): int =
   result = 0
 
 makeRunProc()
