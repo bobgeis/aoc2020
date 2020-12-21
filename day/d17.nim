2afbea0bc1aad0d1cf9787548509a3ec8120f20b
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-20 21:08:10 -0500

    d17: solve part 2, in an ugly way

--- day/d17.nim
+++ day/d17.nim
@@ -5,9 +5,9 @@ const
   testPath = inputPath(day,"t1")
 
 inpath.part1is 388
-# inpath.part2is 2
+inpath.part2is 2280
 testPath.part1is 112
-# testPath.part2is 2
+testPath.part2is 848
 
 proc part0*(path: string): Set3i =
   for y,line in path.getlines:
@@ -50,8 +50,45 @@ proc tick(start:Set3i,iters:int = 1):Set3i =
 proc part1*(input: Set3i): int =
   input.tick(6).len
 
+proc hyperize(input:Set3i):Set4i =
+  for v in input:
+    result.incl v.toVec4
+
+proc getAdj(pos:Vec4i): seq[Vec4i] =
+  for x in -1..1:
+    for y in -1..1:
+      for z in -1..1:
+        for w in -1..1:
+          if not (x == 0 and y == 0 and z == 0 and w == 0):
+            result.add [x,y,z,w]
+
+const adjs4 = [0,0,0,0].getAdj
+
+proc getLive(live:Set4i,peers:Set4i): Set4i =
+  for pos in peers:
+    var sum = 0
+    for adj in adjs4:
+      if live.contains(pos + adj): sum.inc
+    if sum == 3 or ( sum == 2 and live.contains(pos)):
+      result.incl pos
+
+proc getPeers(live:Set4i):Set4i =
+  for pos in live:
+    result.incl pos
+    for adj in adjs4:
+      result.incl( pos + adj )
+
+proc tick(start:Set4i,iters:int = 1):Set4i =
+  var
+    live = start
+    peers = start.getPeers
+  for i in 1..iters:
+    live = getLive(live,peers)
+    peers = live.getPeers
+  live
+
 proc part2*(input: Set3i): int =
-  result = 0
+  input.hyperize.tick(6).len
 
 makeRunProc()
 when isMainModule: getCliPaths(day).doit(it.run.echoRR)
