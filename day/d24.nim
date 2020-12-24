52ee5e9c449a43750a1f75cf7baded14a8170cba
Bob Geis <bobgeis@users.noreply.github.com>
2020-12-24 14:49:43 -0500

    d24: solve part 2

--- day/d24.nim
+++ day/d24.nim
@@ -5,9 +5,9 @@ const
   testPath = inputPath(day,"t1")
 
 inpath.part1is 394
-# inpath.part2is 2
+inpath.part2is 4036
 testPath.part1is 10
-# testPath.part2is 2
+testPath.part2is 2208
 
 proc move(v: Vec2i,dir:string):Vec2i =
   case dir
@@ -35,20 +35,44 @@ proc domoves(ms:string):Vec2i =
     v = v.move(dir)
   v
 
-proc part0*(path: string): seq[string] =
-  path.getLines
-
-proc part1*(input: seq[string]): int =
-  var
-    tiles = initHashSet[Vec2i]()
-  for line in input:
+proc part0*(path: string): Set2i =
+  var tiles = initHashSet[Vec2i]()
+  for line in path.getLines:
     let v = line.domoves
     if v in tiles: tiles.excl v
     else: tiles.incl v
-  tiles.len
+  tiles
 
-proc part2*(input: seq[string]): int =
-  result = 0
+proc part1*(input: Set2i): int =
+  input.len
+
+proc getAdjs(v:Vec2i):array[6,Vec2i] =
+  [[2,0],[-2,0],[1,1],[-1,-1],[1,-1],[-1,1]] + v
+
+proc getPeers(live:Set2i):Set2i =
+  for pos in live:
+    result.incl pos
+    for adj in pos.getAdjs: result.incl adj
+
+proc getLive(live,peers:Set2i):Set2i =
+  for pos in peers:
+    var sum = 0
+    for adj in pos.getAdjs:
+      if adj in live: sum.inc
+    if sum == 2: result.incl pos
+    elif sum == 1 and pos in live: result.incl pos
+
+proc tick(start:Set2i,iters:int = 1):Set2i =
+  var
+    live = start
+    peers = start.getPeers
+  for i in 1..iters:
+    live = getLive(live,peers)
+    peers = live.getPeers
+  live
+
+proc part2*(input: Set2i): int =
+  input.tick(100).len
 
 makeRunProc()
 when isMainModule: getCliPaths(day).doit(it.run.echoRR)
